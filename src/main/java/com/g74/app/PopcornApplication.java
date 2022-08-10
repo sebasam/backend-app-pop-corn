bc32d31e48d397d59e0de8aaf809b61ff863916c
sebasam <sebasamayaxd@hotmail.com>
2022-08-09 19:47:31 -0500

    initial structure

---
+++ src/main/java/com/g74/app/PopcornApplication.java
@@ -0,0 +1,13 @@
+package com.g74.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornApplication.class, args);
+	}
+
+}
